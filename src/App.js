9c18d5118efa0b1112ff4f0468bcae06fdbc812d
Murad <muradteymurov2000@gmail.com>
2023-02-16 15:26:27 +0400

    addevent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 2, 28), 
     },
     {
       id: 'e4',
@@ -34,8 +35,9 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses} />
+      
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,27 @@
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
+import { useState } from 'react';
 
 function ExpenseItem(props) {
+
+
+  const [title, setTitle] = useState(props.title);
+
+  console.log("Exhausted by React!!!")
+
+  const changeTitle = () =>{
+    setTitle("Updated!");
+    console.log(title);
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={changeTitle}>Change</button>
     </Card>
   );
 }

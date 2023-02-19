81e6b378ae93e0e241e252bf3d5622af22295a8f
Murad <muradteymurov2000@gmail.com>
2023-02-19 17:25:01 +0400

    advancedreducer

--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-
-import classes from "./Card.module.css"; 
-
-const Card =(props) => {
-    return <div className={`${classes.card} ${classes.className}`}>{props.children}</div>
-}
-
-export default Card;
